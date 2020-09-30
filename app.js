68cdefb842ffe45b9d79cb453b5c6e219792e534
jianghongbing <jianghongbing1989@qq.com>
2020-09-30 16:26:26 +0800

    add page

--- app.js
+++ app.js
@@ -12,21 +12,22 @@ App({
         wx.showToast({
           title: '登录成功',
         })
-        // console.log(res);
-        // wx.({
-        //   title: JSON.stringify(res),
-        // });
+        console.log(1234);
+        console.log(res);
+        console.log(5678);
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res);
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
+              console.log(res);
               this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -39,6 +40,15 @@ App({
         }
       }
     })
+  },
+  onShow() {
+    console.log('app show');
+  },
+  onHide() {
+    console.log('app hide');
+  },
+  onPageNotFound() {
+    console.log('page not found');
   },
   globalData: {
     userInfo: null
